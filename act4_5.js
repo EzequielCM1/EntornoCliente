a0ee9e31ed5f08401c95dfdca549bd811bf99998
Ezequiel <e.campos.martin@iescristobaldemonroy.es>
2025-10-22 20:57:57 +0200

    Unidad 2
    Estructura de Javascript

---
+++ act4_5.js
@@ -0,0 +1,68 @@
+"use strict";
+
+//////////////////
+//FUNCIONES
+//////////////////
+
+/**
+ * Calcula el volumen de una esfera
+ * @param {number} radio 
+ * @returns number
+ */
+
+function calculaVolumenDeEsfera(radio){
+    return 4/3 * Math.PI * (radio**3)
+}
+
+/**
+ * Recuperar los datos del resultado calcular y mosttrar 
+ */
+function recuperarDatosyCalcular(){
+    // alert(1); sirve para verificar que hace la funcion
+    //1 recuperamos el input donde el uuario ha escrito el radio
+    let txtRadio = document.querySelector("#txtRadio");
+
+    // recuperamos el valor del input
+    const radio = txtRadio.valueAsNumber;
+    // alert(radio);
+
+    //2 Calculamos el volumen de la esfera
+    const volumenEsfera = calculaVolumenDeEsfera(radio);
+
+    // 3 presentar informacion
+    presentarInformacion(radio, volumenEsfera);
+}
+
+/**
+ * Calcula el volumen de una esfera
+ * @param {number} radio 
+ * @param {number} volumenEsfera
+ */
+function presentarInformacion (radio, volumenEsfera) {
+    //1 presentamos ladivision de resultado
+    const divResultado = document.querySelector("#divResultado");
+    //2 creamos la cadena de caracteres que se mostrara en la division
+    const solucion = "<p>Una esfera de radio <code>" + radio + "</code> tiene un volumen de <code>" 
+    + volumenEsfera + "</code> unidades </p>";
+    //3 mostramos las cadenas de caracteres 
+    divResultado.innerHTML = solucion ;
+
+}
+
+
+//////////////////
+//MAIN
+//////////////////
+
+// 1 - Recuperamos el boton del HTML
+const btnCalcular = document.querySelector("#btnCalcular");
+
+//2 Añadimos funcionlidad al boton
+btnCalcular.addEventListener("click", recuperarDatosyCalcular);
+
+/*
+let volumenRadio1 = calculaVolumenDeEsfera(1);
+alert("El volumen de una esfera de radio uno es " + volumenRadio1);
+let volumenRadio2 = calculaVolumenDeEsfera(2);
+alert("El volumen de una esfera de radio dos es " + volumenRadio2);
+*/
